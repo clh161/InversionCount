0819b5bba2639ae2c6a0d5f059e75043db08c1ea
Jacob Ho <clh161@gmail.com>
2017-08-17 23:23:25 +0800

    Bug fix

--- src/main/InversionCount.java
+++ src/main/InversionCount.java
@@ -4,46 +4,47 @@
  */
 public class InversionCount {
 
-    public static int count(int[] arr) {
-        return count(arr, 0, arr.length - 1);
+    public static long count(long[] arr) {
+        long count = count(arr, 0, arr.length - 1);
+        return count;
     }
 
-    public static int count(int[] arr, int left, int right) {
-        int count = 0;
+    public static long count(long[] arr, int left, int right) {
         if (right - left == 0)
             return 0;
         if (right - left == 1) {
             if (arr[left] > arr[right]) {
-                int t = arr[left];
+                long t = arr[left];
                 arr[left] = arr[right];
                 arr[right] = t;
-                count++;
+                return 1;
             }
+            return 0;
         } else {
-            count = count + count(arr, left, left + (right - left) / 2);
-            count = count + count(arr, left + (right - left) / 2, right);
-            int[] temp = new int[right - left + 1];
+            long count = 0;
             int mid = (right - left) / 2 + left;
+            count = count + count(arr, left, mid);
+            count = count + count(arr, mid, right);
+            long[] temp = new long[right - left + 1];
             int k = 0;
             int l = 0;
-            for (int i = left; i < right + 1; i++) {
+            for (long i = left; i < right + 1; i++) {
                 if (left + k == mid) {
                     temp[k + l] = arr[mid + l];
                     l++;
                 } else if (arr[left + k] > arr[mid + l]) {
                     temp[k + l] = arr[mid + l];
                     l++;
-                    count++;
                 } else {
                     temp[k + l] = arr[left + k];
+                    count = count + l;
                     k++;
                 }
             }
             for (int i = 0; i < temp.length; i++) {
                 arr[left + i] = temp[i];
             }
+            return count;
         }
-
-        return count;
     }
 }
--- src/test/InversionCountTest.java
+++ src/test/InversionCountTest.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-import java.io.InputStream;
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,25 +10,56 @@ import static org.junit.Assert.assertEquals;
 public class InversionCountTest {
     @Test
     public void count() {
-        int[] ints = {1};
+        long[] ints = {1};
         assertEquals(0, InversionCount.count(ints));
-        ints = new int[]{1, 2};
+        ints = new long[]{1, 2};
         assertEquals(0, InversionCount.count(ints));
-        ints = new int[]{2, 1};
+        ints = new long[]{2, 1};
         assertEquals(1, InversionCount.count(ints));
-        ints = new int[]{3, 2, 1};
-        assertEquals(3, InversionCount.count(ints));
-        ints = new int[]{3, 1, 2};
+        ints = new long[]{3, 1, 2};
         assertEquals(2, InversionCount.count(ints));
-        ints = new int[]{1, 3, 5, 2, 4, 6};
+        ints = new long[]{1, 3, 5, 2, 4, 6};
         assertEquals(3, InversionCount.count(ints));
-        ints = new int[]{1, 2, 3, 4, 5, 6};
+        ints = new long[]{1, 2, 3, 4, 5, 6};
         assertEquals(0, InversionCount.count(ints));
-        ints = new int[]{2, 4, 1, 3, 5};
+        ints = new long[]{2, 4, 1, 3, 5};
         assertEquals(3, InversionCount.count(ints));
-        ints = new int[]{6, 5, 4, 3, 2, 1};
-        assertEquals(12, InversionCount.count(ints));
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("sample.txt");
+        ints = new long[]{1, 0, 0, 0, 0, 0, 0};
+        assertEquals(6, InversionCount.count(ints));
+        ints = new long[]{0, 0, 0, 0, 0, 0, 0};
+        assertEquals(0, InversionCount.count(ints));
+        int n = 100;
+        long[] ns = new long[n];
+        for (int i = 0; i < n; i++) {
+            ns[i] = n - i;
+        }
+        assertEquals(n * (n - 1) / 2, InversionCount.count(ns));
     }
 
+    @Test
+    public void sorting() throws Exception {
+        int loop = 100;
+        while (loop > 0) {
+            loop--;
+            int len = 100;
+            Random random = new Random();
+            long[] ints = new long[len];
+            for (int i = 0; i < len; i++) {
+                int position = random.nextInt(len - i);
+                for (int j = 0; j < len; j++) {
+                    if (ints[j] == 0) {
+                        if (position == 0) {
+                            ints[j] = i + 1;
+                            break;
+                        }
+                        position--;
+                    }
+                }
+            }
+            InversionCount.count(ints);
+            for (int i = 0; i < ints.length; i++) {
+                assertEquals(i + 1, ints[i]);
+            }
+        }
+    }
 }
